95290d97c188bff700cd9739b98207d16a2aa635
Roman Korobko <roman.korobko@yalantis.com>
2020-04-22 07:32:41 +0300

    Add file uploader and reCaptcha

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry-byebug'
+require 'recaptcha'
+
+Recaptcha.configure do |config|
+  config.site_key  = '6Le7oRETAAAAAETt105rjswZ15EuVJiF7BxPROkY'
+  config.secret_key = '6Le7oRETAAAAAL5a8yOmEdmDi3b2pH7mq5iH1bYK'
+end
+
+include Recaptcha::Adapters::ControllerMethods
+include Recaptcha::Adapters::ViewMethods
 
 get '/' do
   erb :create
@@ -11,6 +21,13 @@ post '/' do
   @email = params[:email]
   @message = params[:message]
 
+  unless params[:file] &&
+         (tmpfile = params[:file][:tempfile]) &&
+         (name = params[:file][:filename])
+    @error = "No file selected"
+    return erb :create
+  end
+
   hh = {
     user_name: "Can't be blank",
     email: "Can't be blank",
@@ -19,7 +36,9 @@ post '/' do
 
   @error = hh.select {|key,_| params[key] == ''}.values.join(", ")
 
-  if @error != ''
+  if @error != '' || !verify_recaptcha
     return erb :create
+  else
+    'Form created'
   end
 end
