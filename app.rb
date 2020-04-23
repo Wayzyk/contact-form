7e4eb43d9b88200d0e2f850a060d3d1588373bb5
Roman Korobko <roman.korobko@yalantis.com>
2020-04-23 09:54:00 +0300

    Add new template for form, finish post action for sending email with data

--- app.rb
+++ app.rb
@@ -1,44 +1,80 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'pry-byebug'
+require 'sinatra/param'
+require 'pony'
 require 'recaptcha'
+require 'pry-byebug'
+require 'rubocop'
+
+include Recaptcha::Adapters::ControllerMethods
+include Recaptcha::Adapters::ViewMethods
 
 Recaptcha.configure do |config|
   config.site_key  = '6Le7oRETAAAAAETt105rjswZ15EuVJiF7BxPROkY'
   config.secret_key = '6Le7oRETAAAAAL5a8yOmEdmDi3b2pH7mq5iH1bYK'
 end
 
-include Recaptcha::Adapters::ControllerMethods
-include Recaptcha::Adapters::ViewMethods
+helpers Sinatra::Param
 
 get '/' do
-  erb :create
+  erb :create, layout: :layout
 end
 
 post '/' do
-  @user_name = params[:user_name]
-  @email = params[:email]
-  @message = params[:message]
+  begin
+    if !verify_recaptcha(action: 'email')
+      raise 'Captcha is not verified'
+    end
 
-  unless params[:file] &&
-         (tmpfile = params[:file][:tempfile]) &&
-         (name = params[:file][:filename])
-    @error = "No file selected"
-    return erb :create
+    validate_email_params
+    unless params[:file].nil?
+      @attachment = { params[:file][:filename] =>
+      params[:file][:tempfile].read }
+    end
+    send_email(params[:email], params[:message], @attachment)
+
+    { status: 200 }.to_json
+  rescue StandardError
+    {
+      status: 500,
+      message: 'Something went wrong'
+    }.to_json
   end
+end
 
-  hh = {
-    user_name: "Can't be blank",
-    email: "Can't be blank",
-    message: 'Too short'
+private
+
+def send_email(from, body, attachment = nil)
+  attachment = {} if attachment.nil?
+  Pony.mail(
+    to: "contact-us@example.com",
+    from: from,
+    subject: "Feedback from #{params[:name]}",
+    body: body,
+    attachments: attachment,
+    via: :smtp,
+    via_options: pony_options
+  )
+end
+
+def pony_options
+  {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: 'user',
+    password: 'password',
+    authentication: :plain,
+    domain: 'localhost.localdomain'
   }
+end
 
-  @error = hh.select {|key,_| params[key] == ''}.values.join(", ")
+def validate_email_params
+  param :name,    String, required: true, min_length: 3, max_length: 250
+  param :email,   String, required: true, format: email_format
+  param :message, String, required: true, min_length: 500
+end
 
-  if @error != '' || !verify_recaptcha
-    return erb :create
-  else
-    'Form created'
-  end
+def email_format
+  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 end
